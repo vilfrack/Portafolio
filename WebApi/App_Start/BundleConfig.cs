9d3ca3249db1b9a09eeb046e24c78f32f44b71ed
Juan villarroel <vilfrack@users.noreply.github.com>
2018-06-08 14:42:15 -0400

    se arregla el estilo

--- WebApi/App_Start/BundleConfig.cs
+++ WebApi/App_Start/BundleConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace Portafolio
+namespace WebApi
 {
     public class BundleConfig
     {
@@ -10,20 +10,6 @@ namespace Portafolio
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/plugin").Include(
-            "~/Scripts/Plugin/skill.js",
-            "~/Scripts/Plugin/jquery.circlechart.js",
-            "~/Scripts/Plugin/jquery.mixitup.js",
-            "~/Scripts/Plugin/custom.js",
-            "~/Scripts/Plugin/smoothscrolling.jquery.js",
-            "~/Scripts/Plugin/jquery.appear.js"));
-
-            bundles.Add(new ScriptBundle("~/main").Include(
-                        "~/Scripts/Plugin/main.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -36,14 +22,7 @@ namespace Portafolio
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/styles.css"));
-
-            bundles.Add(new StyleBundle("~/Content/Bars").Include(
-              "~/Content/bars.css"));
-
-
-
-
+                      "~/Content/site.css"));
         }
     }
 }
