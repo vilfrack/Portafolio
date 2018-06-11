6b7de5b933681603a353057ae0e5104be1175da8
Juan villarroel <vilfrack@users.noreply.github.com>
2018-06-10 20:54:32 -0400

    s

--- Portafolio/Controllers/PerfilController.cs
+++ Portafolio/Controllers/PerfilController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Portafolio.Models;
+using Portafolio.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +12,55 @@ namespace Portafolio.Controllers
     public class PerfilController : Controller
     {
         // GET: Perfil
+        PortafolioEntities Entities = new PortafolioEntities();
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult Add() {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Add(ViewModelPerfil perfil)
+        {
+            if (!ModelState.IsValid)
+            {
+                return PartialView();
+            }
+            Perfil p = new Perfil();
+            p.Extracto = perfil.Extracto;
+            p.Titular = perfil.Titular;
+            p.ImgExtracto = SaveUploadedFile(perfil.imgExtracto);
+            p.ImgTitular = SaveUploadedFile(perfil.imgTitular);
+            Entities.Perfil.Add(p);
+            Entities.SaveChanges();
+            return PartialView();
+        }
+
+        public ActionResult Details() {
+            return PartialView();
+        }
+
+        private string SaveUploadedFile(HttpPostedFileBase file)
+        {
+            string ruta_virtual = "";
+            if (file != null)
+            {
+                string ruta = Path.Combine(Server.MapPath("~/Images/"));
+                ruta_virtual = "~/Images/"+ file.FileName + "";
+               // string pathString = ruta;
+                bool isExists = System.IO.Directory.Exists(ruta);
+
+                if (!isExists)
+                    System.IO.Directory.CreateDirectory(ruta);
+
+                var path = string.Format("{0}\\{1}", ruta, file.FileName);
+                file.SaveAs(path);
+                return ruta_virtual;
+            }
+            return ruta_virtual;
+        }
     }
 }
---
+++ Portafolio/ViewModel/ViewModelPerfil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Portafolio.ViewModel
+{
+    public class ViewModelPerfil
+    {
+        [Required(ErrorMessage = "El campo {0} no puede quedar vacio")]
+        public string Titular { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} no puede quedar vacio")]
+        public string Extracto { get; set; }
+
+        public HttpPostedFileBase imgTitular { get; set; }
+
+        public HttpPostedFileBase imgExtracto { get; set; }
+    }
+}
